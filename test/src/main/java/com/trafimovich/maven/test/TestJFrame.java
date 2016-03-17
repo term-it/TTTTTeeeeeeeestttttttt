d0dfe082f52997df11287904702e86601ea7a051
Mikalai Trafimovich <termit1062@gmail.com>
2016-03-17 11:53:35 +0300

    test11111111

---
+++ test/src/main/java/com/trafimovich/maven/test/TestJFrame.java
@@ -0,0 +1,47 @@
+package com.trafimovich.maven.test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JProgressBar;
+
+public class TestJFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestJFrame frame = new TestJFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestJFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 274, 206);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(50);
+		contentPane.add(progressBar, BorderLayout.CENTER);
+	}
+
+}
